b8bb0b89889fcb796b6dde2492c968c6981e0d6c
Ernest Guevarra <ernest@guevarra.io>
2020-08-09 01:19:27 +0100

    initial commit

---
+++ R/psa.R
@@ -0,0 +1,19 @@
+################################################################################
+#
+#'
+#' Utilities to Access Data from the Philippine Statistics Authority
+#'
+#' The Philippine Statistics Authority is the primary statistical arm of the
+#' Philippine government. It is responsible in the conduct and content of all
+#' national census and surveys, gathering of sectoral statistics, consolidation
+#' of selected administrative recording systems, and compilation of national
+#' accounts. As part of the Open Data Initiative, the PSA makes its datasets
+#' publicly and widely availabe.
+#'
+#' @docType package
+#' @name psa
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
---
+++ data-raw/vitals.R
@@ -0,0 +1,40 @@
+## code to prepare `vitals` dataset goes here
+
+## Libraries
+library(openxlsx)
+
+## Read data
+d2015 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2015.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+d2016 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2016.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+d2017 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2017.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+d2018 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2018.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+d2019 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2019.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+d2020 <- read.xlsx(xlsxFile = "data-raw/vitals/deaths2020.xlsx",
+                   startRow = 6, colNames = FALSE)
+
+names(d2020) <- c("residence", "total", "january", "february", "march", "april",
+                  "may", "june")
+
+names(d2015) <- c("residence", "total", "january", "february", "march", "april",
+                  "may", "june", "july", "august", "september", "october",
+                  "november", "december")
+
+names(d2019) <- names(d2018) <- names(d2017) <- names(d2016) <- names(d2015)
+
+
+
+
+
+
+
+usethis::use_data(vitals, overwrite = TRUE)
